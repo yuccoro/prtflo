13592a7d34f8202a9f6677818089ce8d6076be24
yuccoro <yuccorobe@gmail.com>
2022-08-07 14:25:46 +0900

    ragdollを追加

---
+++ ragdoll/js/main.js
@@ -0,0 +1,43 @@
+'use strict'
+
+{
+  /*------------------------------
+    スライドショー
+  ------------------------------*/
+  const pic_array = ["img/hero01.jpg", "img/hero02.jpg", "img/hero03.jpg", "img/hero04.jpg", "img/hero05.jpg",];
+  let slide_count = 0;
+
+  function playSlideshow() {
+    if (slide_count === pic_array.length) {
+      slide_count = 0;
+    } else {
+      slide_count++;
+    }
+
+    document.querySelector('#hero img').setAttribute('src', pic_array[slide_count]);
+  }
+
+  setInterval(playSlideshow, 5000);
+
+  /*------------------------------
+    ボタン画像のマウスオーバー
+  ------------------------------*/;
+  const btns = (document.querySelectorAll('.btn img'));
+
+  function addSrcOn(src) {
+    const dot = src.indexOf('.');
+    return `${src.substring(0, dot)}_on${src.substring(dot)}`;
+  }
+
+  btns.forEach(btn => {
+    const default_src = (btn.getAttribute('src'));
+
+    btn.addEventListener('mouseover', function() {
+      btn.setAttribute('src', addSrcOn(default_src));
+    })
+
+    btn.addEventListener('mouseout', function() {
+      btn.setAttribute('src', default_src);
+    })
+  });
+}
