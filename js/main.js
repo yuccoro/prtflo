82d2b5fe806c76afc8baecb31d95836f81a69032
yuccoro <yuccorobe@gmail.com>
2022-08-17 22:51:14 +0900

    sp-menu実装

--- js/main.js
+++ js/main.js
@@ -1,11 +1,38 @@
 'use strict'
 
-$(() => {
-  var scroll = new SmoothScroll('a[href*="#"]', {
-    speed: 1000
-  });
-});
+{
+  /*--------------------------------------------------
+    ハンバーガーメニュー開閉
+  --------------------------------------------------*/
+  const toggle = document.querySelector('header .toggle');
+  const nav = document.querySelector('header nav');
+  const links = document.querySelectorAll('header nav ul li a');
 
-$(function() {
-  window.contact.checkValidation()
-});
+  const toggleClass = function(el, className) {
+    el.classList.toggle(className);
+  };
+
+  toggle.addEventListener('click', () => {
+    toggleClass(toggle, 'open');
+    toggleClass(nav, 'show');
+  });
+
+  links.forEach(link => {
+    link.addEventListener('click', () => {
+      toggle.classList.remove('open');
+      nav.classList.remove('show');
+    });
+  });
+
+
+  $(() => {
+    var scroll = new SmoothScroll('a[href*="#"]', {
+      speed: 1000
+    });
+  });
+
+  
+  $(function() {
+    window.contact.checkValidation()
+  });
+}
