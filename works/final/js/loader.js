04dfdee7e5347400e5245f357257e37eee453677
yuccoro <yuccorobe@gmail.com>
2022-08-28 01:36:31 +0900

    final modify

---
+++ works/final/js/loader.js
@@ -0,0 +1,6 @@
+'use strict'
+
+window.onload = () => {
+  const loader = document.getElementById('loader');
+  loader.classList.add('loaded');
+}
--- works/final/js/main.js
+++ works/final/js/main.js
@@ -3,7 +3,6 @@
 {
   const toggle = document.getElementById('toggle');
   const ul = document.querySelector('nav ul');
-  console.log(toggle);
 
   toggle.addEventListener('click', function() {
     toggle.classList.toggle('open');
